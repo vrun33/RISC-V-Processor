9338d4b7c7dc45793241d58d191685150ea8093f
IamSid44 <siddarth.g@students.iiit.ac.in>
2025-02-19 14:36:57 +0530

    Adding seq_processor template

--- seq_processor.v
+++ seq_processor.v
@@ -1,10 +1,37 @@
 // Wrapper conencting all the blocks
 
+`include "pc.v"
+`include "instruction_mem.v"
+`include "control.v"
+`include "imm_gen.v"
+`include "register_file.v"
+`include "data_memory.v"
+`include "MUX_2X1.v"
+`include "CLA_32_BIT.v"
+`include "and2.v"
+`include "sl1.v"
+// Include ALU
+
 module seq_processor (
     input clk
 );
 
-    // Read from memory in initial begin
+    // Instantiate Hardware
+    initial
+    begin
 
+    end
+
+    // Instantiate Memory
+    initial
+    begin
+
+    end
+
+    // Update on Clock
+    always @(posedge clk)
+    begin
+
+    end
 
 endmodule
