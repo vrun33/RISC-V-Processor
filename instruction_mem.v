edfcb3c273a5a9036ecea4bacb0793c12cc861d0
IamSid44 <siddarth.g@students.iiit.ac.in>
2025-02-19 16:59:45 +0530

    Test Case 2

--- instruction_mem.v
+++ instruction_mem.v
@@ -1,6 +1,6 @@
 module instruction_memory #(
     parameter MEM_SIZE = 4095,
-    parameter MEM_INIT_FILE = "imemory_2.txt"
+    parameter MEM_INIT_FILE = "imemory.txt"
     // parameter MEM_INIT_FILE = "imemory.txt"
 ) (
     input wire clk,
