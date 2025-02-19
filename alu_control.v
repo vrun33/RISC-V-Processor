b52e568bdc707e0d8608c4eb944624b267dd4a2f
Varun Shastry <varun.shastry@research.iiit.ac.in>
2025-02-19 14:20:12 +0530

    Changed alu_control.v cuz sammu moment

--- alu_control.v
+++ alu_control.v
@@ -1,20 +1,15 @@
-// op_3 = 0
-// op_2 = alu_op_0 + (alu_op_1 * i_30 * ~i_12 * ~i_13 * ~i_14)
-// op_1 = (~alu_op_0 * ~alu_op_1) + (alu_op_0) + (alu_op_1 * ~i_12 * ~i_13 * ~i_14)
-// op_0 = alu_op_1 * ~i_30 * ~i_12 * i_13 * i_14
-
 module alu_control (
-    input wire alu_op_1,
-    input wire alu_op_0,
-    input wire i_30,
-    input wire i_14,
-    input wire i_13,
-    input wire i_12,
-    output wire op_3,
-    output wire op_2,
-    output wire op_1,
-    output wire op_0
+    input wire [1:0] alu_op,      // {alu_op_1, alu_op_0}
+    input wire [3:0] instr_bits,   // {i_30, i_14, i_13, i_12}
+    output wire [3:0] op          // {op_3, op_2, op_1, op_0}
 );
+    wire alu_op_1 = alu_op[1];
+    wire alu_op_0 = alu_op[0];
+    wire i_30 = instr_bits[3];
+    wire i_14 = instr_bits[1];
+    wire i_13 = instr_bits[2];
+    wire i_12 = instr_bits[0];
+    
     // Gate level implementation of the ALU control logic
     wire temp1, temp2, temp3, temp4, temp5, temp6, temp7, temp8, temp9;
 
@@ -29,8 +24,8 @@ module alu_control (
     and (temp8, temp1, temp2);                         // ~alu_op_0 & ~alu_op_1
     and (temp9, alu_op_1, temp3, temp4, temp5);        // alu_op_1 & ~i_12 & ~i_13 & ~i_14
 
-    assign op_3 = 1'b0;
-    or (op_2, alu_op_0, temp7);
-    or (op_1, temp8, alu_op_0, temp9);
-    and (op_0, alu_op_1, temp6, temp3, i_13, i_14);
+    assign op[3] = 1'b0;
+    or (op[2], alu_op_0, temp7);
+    or (op[1], temp8, alu_op_0, temp9);
+    and (op[0], alu_op_1, temp6, temp3, i_13, i_14);
 endmodule
