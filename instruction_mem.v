13790740b2c26f16e15876c78afcd6ca5192d90e
Varun Shastry <varun.shastry@research.iiit.ac.in>
2025-02-19 18:11:57 +0530

    Added supporting instructions

--- instruction_mem.v
+++ instruction_mem.v
@@ -1,6 +1,6 @@
 module instruction_memory #(
     parameter MEM_SIZE = 4095,
-    parameter MEM_INIT_FILE = "test_program_2.txt"
+    parameter MEM_INIT_FILE = "test_program_4.txt"
     // parameter MEM_INIT_FILE = "imemory.txt"
 ) (
     input wire clk,
