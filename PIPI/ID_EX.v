ed79ea45168eff51c701d508cffc2f0ac91636ae
mpsamartha <m.samartha@students.iiit.ac.in>
2025-03-05 22:10:22 +0530

    Changed ID_EX.v

--- PIPI/ID_EX.v
+++ PIPI/ID_EX.v
@@ -4,32 +4,34 @@
 module ID_EX(
     input wire clk,
     input wire reset,
+    input wire mem_to_reg,
+    input wire reg_write_en,
+    input wire mem_read,
+    input wire mem_write,
+    input wire branch,
+    input wire [3:0] alu_control,
+    input wire alu_src,
+    input wire [63:0] ID_EX_pc_in,
     input wire [63:0] data_in_1,
     input wire [63:0] data_in_2,
+    input wire [63:0] imm_gen,
     input wire [4:0] ID_EX_rs1,
     input wire [4:0] ID_EX_rs2,
     input wire [4:0] ID_EX_rd,
-    input wire mem_read,
-    input wire mem_to_reg,
-    input wire reg_write_en,
-    input wire [3:0] alu_control,
-    input wire mem_write,
-    input wire alu_src,
-    input wire branch,
-    input wire [63:0] imm_gen,
-    output wire [63:0] read_data1,
-    output wire [63:0] read_data2,
-    output wire mem_read_out,
     output wire mem_to_reg_out,
     output wire reg_write_en_out,
-    output wire [3:0] alu_control_out,
+    output wire mem_read_out,
     output wire mem_write_out,
+    output wire branch_out
+    output wire [3:0] alu_control_out,
     output wire alu_src_out,
+    output wire [63:0] ID_EX_pc_out,
+    output wire [63:0] read_data1,
+    output wire [63:0] read_data2,
     output wire [63:0] imm_gen_out
     output wire [4:0] ID_EX_rs1_out,
     output wire [4:0] ID_EX_rs2_out,
     output wire [4:0] ID_EX_rd_out,
-    output wire branch_out
 );
 
     reg [63:0] reg_data_in_1;
@@ -45,6 +47,22 @@ module ID_EX(
     reg alu_src_reg;
     reg branch_reg;
     reg [63:0] imm_gen_reg;
+    reg [63:0] ID_EX_pc_reg;
+
+    assign read_data1 = reg_data_in_1;
+    assign read_data2 = reg_data_in_2;
+    assign ID_EX_rs1_out = reg_ID_EX_rs1;
+    assign ID_EX_rs2_out = reg_ID_EX_rs2;
+    assign ID_EX_rd_out = reg_ID_EX_rd;
+    assign mem_read_out = mem_read_reg;
+    assign mem_to_reg_out = mem_to_reg_reg;
+    assign reg_write_en_out = reg_write_en_reg;
+    assign alu_control_out = alu_control_reg;
+    assign mem_write_out = mem_write_reg;
+    assign alu_src_out = alu_src_reg;
+    assign branch_out = branch_reg;
+    assign imm_gen_out = imm_gen_reg;
+    assign ID_EX_pc_out = ID_EX_pc_reg;
 
     always @(posedge clk or posedge reset) begin
         if (reset) begin
@@ -61,6 +79,7 @@ module ID_EX(
             alu_src_reg <= 1'b0;
             branch_reg <= 1'b0;
             imm_gen_reg <= 64'b0;
+            ID_EX_pc_reg <= 64'b0;
         end else begin
             reg_data_in_1 <= data_in_1;
             reg_data_in_2 <= data_in_2;
@@ -75,21 +94,8 @@ module ID_EX(
             alu_src_reg <= alu_src;
             branch_reg <= branch;
             imm_gen_reg <= imm_gen;
+            ID_EX_pc_reg <= ID_EX_pc_in;
         end
     end
-
-    assign read_data1 = reg_data_in_1;
-    assign read_data2 = reg_data_in_2;
-    assign ID_EX_rs1_out = reg_ID_EX_rs1;
-    assign ID_EX_rs2_out = reg_ID_EX_rs2;
-    assign ID_EX_rd_out = reg_ID_EX_rd;
-    assign mem_read_out = mem_read_reg;
-    assign mem_to_reg_out = mem_to_reg_reg;
-    assign reg_write_en_out = reg_write_en_reg;
-    assign alu_control_out = alu_control_reg;
-    assign mem_write_out = mem_write_reg;
-    assign alu_src_out = alu_src_reg;
-    assign branch_out = branch_reg;
-    assign imm_gen_out = imm_gen_reg;
 
 endmodule
