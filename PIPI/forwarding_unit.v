41024b87ca8b7470d24bd5214a4c6dd176df2b6a
Varun Shastry <varun.shastry@research.iiit.ac.in>
2025-03-05 22:55:38 +0530

    Corrected and pushed forwarding_unit.v

--- PIPI/forwarding_unit.v
+++ PIPI/forwarding_unit.v
@@ -14,41 +14,47 @@ module forwarding_unit(
     // 01 = Forward from MEM/WB stage
     // 10 = Forward from EX/MEM stage
 
+    reg [1:0] ForwardA_reg, ForwardB_reg;
+
     always @(*) begin
 
         // Assume no forwarding
-        ForwardA = 2'b00;
-        ForwardB = 2'b00;
+        ForwardA_reg = 2'b00;
+        ForwardB_reg = 2'b00;
         
         // EX hazard for rs1 (ForwardA)
         if (EX_MEM_reg_write_en && 
             (EX_MEM_rd != 5'b0) &&
             (EX_MEM_rd == ID_EX_rs1)) begin
-            ForwardA = 2'b10;  
+            ForwardA_reg = 2'b10;  
         end
 
         // MEM hazard for rs1 (ForwardA)
-        else if (MEM_WB_reg_write_en, && 
+        else if (MEM_WB_reg_write_en && 
                  (MEM_WB_rd != 5'b0) &&
                  !(EX_MEM_reg_write_en && (EX_MEM_rd != 5'b0) && (EX_MEM_rd == ID_EX_rs1)) && // give priority to EX/MEM stage
                  (MEM_WB_rd == ID_EX_rs1)) begin
-            ForwardA = 2'b01;  // from MEM/WB 
+            ForwardA_reg = 2'b01;  // from MEM/WB 
         end
         
         // EX hazard for rs2 (ForwardB)
         if (EX_MEM_reg_write_en && 
             (EX_MEM_rd != 5'b0) &&
             (EX_MEM_rd == ID_EX_rs2)) begin
-            ForwardB = 2'b10;  // from EX/MEM 
+            ForwardB_reg = 2'b10;  // from EX/MEM 
         end
         // MEM hazard for rs2 (ForwardB)
-        else if (MEM_WB_reg_write_en, && 
+        else if (MEM_WB_reg_write_en && 
                  (MEM_WB_rd != 5'b0) &&
                  !(EX_MEM_reg_write_en && (EX_MEM_rd != 5'b0) && (EX_MEM_rd == ID_EX_rs2)) && // give priority to EX/MEM stage
                  (MEM_WB_rd == ID_EX_rs2)) begin
-            ForwardB = 2'b01;  // from MEM/WB
+            ForwardB_reg = 2'b01;  // from MEM/WB
         end
     end
+
+    assign ForwardA = ForwardA_reg;
+    assign ForwardB = ForwardB_reg;
+
 endmodule
 
 // #TODO : ld followed by stor and vice versa
